e514545cc8bed0d6358b8d006adb0991b8e9b797
colinporth <colinwrey@hotmail.com>
2017-03-02 17:12:19 +0000

    Revert "fiddle"
    This reverts commit 2ea6d90231c0d2f5c67c6d0925cfe497a127e38e.

--- cKeyboard.h
+++ cKeyboard.h
@@ -3,6 +3,28 @@
 #include <termios.h>
 #include <map>
 
+//{{{
+class cKeyConfig {
+public:
+  enum { ACTION_EXIT, ACTION_PLAYPAUSE,
+         ACTION_DECREASE_VOLUME, ACTION_INCREASE_VOLUME,
+         ACTION_SEEK_BACK_SMALL, ACTION_SEEK_FORWARD_SMALL,
+         ACTION_SEEK_BACK_LARGE, ACTION_SEEK_FORWARD_LARGE,
+         ACTION_STEP,
+         ACTION_PREVIOUS_AUDIO, ACTION_NEXT_AUDIO,
+         ACTION_PREVIOUS_VIDEO, ACTION_NEXT_VIDEO,
+         };
+
+  #define KEY_LEFT 0x5b44
+  #define KEY_RIGHT 0x5b43
+  #define KEY_UP 0x5b41
+  #define KEY_DOWN 0x5b42
+  #define KEY_ESC 27
+
+  static std::map<int, int> buildDefaultKeymap();
+  };
+//}}}
+
 class cKeyboard : public cOmxThread {
 public:
   cKeyboard();
